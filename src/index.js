51a0d8ec084f0306c4ecaa67b011cf3c3b28ba80
Vincent <vshan0816@gmail.com>
2023-08-21 15:32:36 -0400

    messing with creating circle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("Webpack is working!");
+
+import MovingObject from "./moving-object.js"
+window.MovingObject = MovingObject
+
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
