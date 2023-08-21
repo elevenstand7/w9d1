ebdfe6da4ca6df085a9a57f430a51b9d9b84614b
Vincent <vshan0816@gmail.com>
2023-08-21 16:36:29 -0400

    Finish circle

--- src/moving-object.js
+++ src/moving-object.js
@@ -1,18 +1,25 @@
 class MovingObject {
     constructor(hash) {
-       for (let key in hash){
-        this[key] = hash[key]
-       }
+      for(let key in hash){
+        this[key] = hash[key];
+      }
+      // this.pos = hash.pos;
+      // this.vel = hash.vel;
+      // this.radius = hash.radius;
+      // this.color = hash.color;
     }
-
     draw(ctx) {
-        
-
-        ctx.beginPath();
-        ctx.arc(this[pos][0], this[pos][1], this[radius], 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fillStyle = this[color]
+      // let that = this;
+      // debugger;
+      ctx.beginPath();
+      // debugger;
+      let x = this.pos[0];
+      let y = this.pos[1];
+      let r = this.radius;
+      ctx.arc(x, y, r, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fillStyle = this.color;
     }
-}
-
-export default MovingObject;
+  }
+  export default MovingObject;
+  // module.exports = MovingObject;
